40497be951bdd92a494eb361c36635bb8f0bc2a3
RominaCalaniCruz <cruznoemi220@gmail.com>
2022-07-05 18:05:07 -0300

    clase 2

--- 1-EntradaSalida/jsEntradaSalida-09.js
+++ 1-EntradaSalida/jsEntradaSalida-09.js
@@ -1,10 +1,18 @@
 /*
+Romina Calani Cruz
+DIV X
+Enunciado:
 Debemos lograr tomar el importe por ID ,
 transformarlo a entero (parseInt), luego
 mostrar el importe con un aumento del 10 %
 en el cuadro de texto "RESULTADO".*/
 function mostrarAumento()
 {
+	let sueldo;
+	let resultado;
 
-	alert("ok");
+	sueldo = parseInt(document.getElementById("txtIdSueldo").value);
+	resultado = sueldo + ((sueldo * 10)/100);
+	document.getElementById("txtIdResultado").value = resultado;
+
 }
--- 1-EntradaSalida/jsEntradaSalida-10.js
+++ 1-EntradaSalida/jsEntradaSalida-10.js
@@ -1,9 +1,18 @@
 /*
+Romina Calani Cruz
+DIV X
+Enunciado:
 Debemos lograr tomar el importe por ID.
 Transformarlo a entero (parseInt), luego
 mostrar el importe con un Descuento del 25 %
 en el cuadro de texto "RESULTADO"*/
 function mostrarAumento()
 {
-	alert("ok");
+	let importe;
+	let resultado;
+
+	importe = document.getElementById("txtIdImporte").value;
+	resultado = importe - ((importe * 25)/100);
+	
+	document.getElementById("txtIdResultado").value = resultado;
 }
